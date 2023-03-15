7e80aa2c4caa462610bfc6b2c23de54876696c9f
SENACRS <senacrs@fspoa.br>
2023-03-15 20:43:32 -0300

    primeiro commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+require('dotenv/config')
+
+class AppController {
+    constructor() {
+        this.app = express();
+
+        this.middlewares()
+        this.routes()
+    }
+    middlewares() {
+        this.app.use(express.json())
+        // MOSTRAR LOG REQUESTS
+        this.app.use(morgan('tiny'))
+        this.app.use(cors())
+        this.app.options('*', cors())
+    }
+    routes() {
+        //this.app.use(require('./routes/Users'))
+    }
+}
+
+module.exports = new AppController().app
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const app = require('./app')
+//const mongoose = require('mongoose')
+
+console.log('\n============================ Prog Fullstack ============================')
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server rodando na porta 3000 ✔️`)
+})
+
